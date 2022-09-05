0c84925ab28274d904f95aea839bf6818741874a
muhittinayri <ayri.muhittin@gmail.com>
2022-09-06 01:00:12 +0300

    Erişim Belirleyiciler (Access Modifiers) konusu ile ilgili konu özeti eklenmiştir.

---
+++ src/accessModifiers/AcccessModifiers.java
@@ -0,0 +1,45 @@
+package accessModifiers;
+
+public class AcccessModifiers {
+/*
+
+### Erişim Belirleyiciler(Access Modifiers)
+
+##### Büyük projelerde karmaşıklığı ortadan kaldırdığı gibi kolaylıkta sağlar. Paket yapısında esas amaç mantıksal birlikteliği
+##### olan sınıfları aynı paket altında toplamaktır. Paket yapıları kurulduktan, sınıflar bu paketler altında yerlerini aldıktan,
+##### ve sınıflar içerisinde farklı görevlere sahip metotlar oluşturulduktan sonra sınıfların farklı sınıflardaki metotlara erişim
+##### sağlaması gerekmete veya bir sınıftaki özel bir metoda başka hiç bir sınıfın erişememesi gerekmektedir. İşte bu gibi durumları
+##### belirlediğimiz yapı **Erişim belirleyicilerdir (Access Modifiers)** Javada erişim belirleyiciler 4 adettir.
+
+##### public
+##### protected
+##### private
+##### friendly (Erişim belirleyici yok)
+
+## Public Erişim Belirleyici
+
+##### Ataması yapılan değişken, metot veya sınıfın program üzerinde bulunan bütün paketlere erişimi açık bir şekilde olur.
+##### Yani programın üzerindeki her alandan verilere erişim sağlanabilir, değiştirilebilir ve silinebilir.
+##### Bu yüzden global çaplı projeleri tasarlarken public erişim seviyesini kullanmaktan çekinmeliyiz.
+
+## Protected Erişim Belirleyici
+
+##### Bir sınıf üzerinde gerçekleştirdiğimiz tanımlamalar sonucunda üretilen değişkenler ve metotların kaydını
+##### yaptığımız sınıftan üretilen alt sınıflar ve yer aldığı paket üzerindeki diğer sınıflardan kullanılmasına
+##### olanak sağlayan bir erişim seviyesidir.
+
+## Private Erişim Belirleyici
+
+##### Bazen güvenlik amacı ile değişkenlerimizin, metotlarımızın ve sınıf yapılarımızın içerisindeki yapıları
+##### gizlemek isteyebiliriz. Bu isteğimizi yerine getirmek için kullanacağımız private erişim belirleyicisi
+##### sayesinde sadece kullanılan sınıf içerisinde erişim sağlanabilir
+
+## Default / Friendly Erişim Belirleyici
+
+##### Diğer erişim belirleyicilerinin aksine bu erişim seviyemizin bir anahtar kelimesi yoktur. Yani herhangi
+##### bir değişken tanımlamasında, metot veya sınıf oluşumunda herhangi bir erişim belirleyicisi tanımlaması
+##### yapmazsak programımız onu “default / friendly” erişim seviyesinde algılamaktadır. Bu erişim seviyesinde
+##### tanımlamalar sınıf içerisinden, dış alt sınıf oluşumlarından ve aynı paket üzerinden erişime açık olurlar.
+
+ */
+}
