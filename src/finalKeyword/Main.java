fa7cde3c0783d91b17e0ebe9ea43e9322db634a7
muhittinayri <ayri.muhittin@gmail.com>
2022-09-06 22:30:10 +0300

    Final konusu ile ilgili konu özeti eklenmiştir.

---
+++ src/finalKeyword/Main.java
@@ -0,0 +1,33 @@
+package finalKeyword;
+
+public class Main {
+    /*
+
+    Final Değişkenler
+
+    Final olarak tanımlanan değişkenler ilk değerlerini aldıktan sonra değiştirilemezler.
+    Bununla birlikte final değişkenlere ilk değer ataması ya tanımlandığı satırda ya da yapıcı
+     metot içerisinde yapılmalıdır. Bunun anlamı şudur: nesnenin yaratılması işi bittiğinde final
+     değişkenimizin değeri belli olacaktır ve bir daha değiştirilemeyecektir.
+
+     Final değişkenlerin değerleri ilk değer ataması yapıldıktan sonra değiştirilemez.
+      Zaten final değişkenlerin kullanım alanı da budur. Değerinin değişmesini istemediğimiz
+      bir değiikeni final anahtar kelimesi ile tanımlarız.
+
+      Sabitler okunabilirliği artırmak amacıyla büyük harflerle tanımlanırlar.
+
+
+
+      Final Metodlar
+
+      Final belirteci ile yazılan metodlar alt sınıflar tarafından override edilemezler.
+      Alt sınıflarda override edilerek değiştirilmesini istemediğimiz metodları final olarak tanımlayabiliriz.
+
+
+
+      Final Sınıflar
+      Final anahtar kelimesi sınıflara da uygulanabilmektedir. Bir sınıf aşağıdaki gibi
+      final anahtar kelimesi kullanılarak tanımlanıyorsa bu sınıfı kalıtmak mümkün olmaz
+
+     */
+}
